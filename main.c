f430f64f5782c6a2634e440881ee3f872a33ecca
Vladimir Zidar <mr_w@mindnever.org>
2018-01-18 01:03:16 +0100

    Added preheat state, reformatted source.

--- main.c
+++ main.c
@@ -23,9 +23,11 @@
 #define SENSOR_B_ADCMUX 5
 #define SENSOR_B_DIDR0 5
 
+// reset button
 #define BUTTON_A_PORT F
 #define BUTTON_A_PIN 6
 
+// external thermostat trigger
 #define BUTTON_B_PORT B
 #define BUTTON_B_PIN 2
 
@@ -69,13 +71,14 @@
 #define TARGET_OIL_TEMPERATURE 90.0
 #define TARGET_WATER_TEMPERATURE 60.0
 
-#define TEMP_HYST 2
+#define OIL_TEMP_HYST 2
+#define WATER_TEMP_HYST 15
 
 #define IS_OIL_HOT() (oil_temperature > target_oil_temperature)
-#define IS_OIL_COLD() (oil_temperature < (target_oil_temperature - TEMP_HYST))
+#define IS_OIL_COLD() (oil_temperature < (target_oil_temperature - OIL_TEMP_HYST))
 
 #define IS_WATER_HOT() (water_temperature > target_water_temperature)
-#define IS_WATER_COLD() (water_temperature < (target_water_temperature - TEMP_HYST))
+#define IS_WATER_COLD() (water_temperature < (target_water_temperature - WATER_TEMP_HYST))
 
 #define IS_PRESSED(b) ((b) == BUTTON_DEBOUNCE)
 
@@ -86,6 +89,8 @@
 #define TIMEOUT_SPARK (4000 / TICK_MS)
 #define TIMEOUT_FLAME (4000 / TICK_MS)
 #define DELAY_STATUS (500/TICK_MS)
+
+#define LCD_REINIT_COUNT 10
 
 #define IGNITION_RETRY 3
 
@@ -127,33 +132,35 @@
 
 typedef enum
 {
-  state_init,
-  state_wait,
-  state_fan,
-  state_air,
-  state_spark,
-  state_detect_flame,
-  state_burn,
-  state_fault,
+    state_init,
+    state_wait,
+    state_preheat,
+    state_fan,
+    state_air,
+    state_spark,
+    state_detect_flame,
+    state_burn,
+    state_fault,
 } state_t;
 
 static const char *state_name[] = {
-  "Init",
-  "Wait",
-  "Fan",
-  "Air",
-  "Spark",
-  "Flame?",
-  "Burn",
-  "Fault"
+    "Init",
+    "Wait",
+    "Prheat",
+    "Fan",
+    "Air",
+    "Spark",
+    "Flame?",
+    "Burn",
+    "Fault",
 };
 
 typedef enum
 {
-  blink_off,
-  blink_slow,
-  blink_fast,
-  blink_on
+    blink_off,
+    blink_slow,
+    blink_fast,
+    blink_on
 } blink_t;
 
 static float target_oil_temperature = TARGET_OIL_TEMPERATURE;
@@ -164,360 +171,374 @@ static uint8_t led_b = blink_off;
 
 uint8_t led_state(blink_t b, uint8_t slow, uint8_t fast)
 {
-  if(b == blink_fast) { b = fast ? blink_on : blink_off; }
-  if(b == blink_slow) { b = slow ? blink_on : blink_off; }
-  
-  return b == blink_on;
+    if(b == blink_fast) { b = fast ? blink_on : blink_off; }
+    if(b == blink_slow) { b = slow ? blink_on : blink_off; }
+    
+    return b == blink_on;
 }
 
 void handle_led()
 {
-  static uint8_t fast = 0;
-  static uint8_t slow = 0;
-  static uint16_t tick_f = 0, tick_s = 0;
-
-  if(++tick_f > TICK_FAST) {
-    fast = !fast;
-    tick_f = 0;
-  }
-  
-  if(++tick_s > TICK_SLOW) {
-    slow = !slow;
-    tick_s = 0;
-  }
-
-  if(led_state( led_a, slow, fast)) {
-    LED_ON( LED_A );
-  } else {
-    LED_OFF( LED_A );
-  }
-  
-  if(led_state( led_b, slow, fast)) {
-    LED_ON( LED_B );
-  } else {
-    LED_OFF( LED_B );
-  }
+    static uint8_t fast = 0;
+    static uint8_t slow = 0;
+    static uint16_t tick_f = 0, tick_s = 0;
+    
+    if(++tick_f > TICK_FAST) {
+        fast = !fast;
+        tick_f = 0;
+    }
+    
+    if(++tick_s > TICK_SLOW) {
+        slow = !slow;
+        tick_s = 0;
+    }
+    
+    if(led_state( led_a, slow, fast)) {
+        LED_ON( LED_A );
+    } else {
+        LED_OFF( LED_A );
+    }
+    
+    if(led_state( led_b, slow, fast)) {
+        LED_ON( LED_B );
+    } else {
+        LED_OFF( LED_B );
+    }
 }
 
 static void adc_mux(uint8_t mux)
 {
-  ADMUX = _BV( REFS0 ) | _BV( ADLAR ) | mux;
+    ADMUX = _BV( REFS0 ) | _BV( ADLAR ) | mux;
 }
 
 static void init_sensors()
 {
 #ifdef SENSOR_A_PORT
-  IO_DIR_IN( SENSOR_A );
-  IO_PIN_HIGH( SENSOR_A ); // pullup
+    IO_DIR_IN( SENSOR_A );
+    IO_PIN_HIGH( SENSOR_A ); // pullup
 #endif
-
+    
 #ifdef SENSOR_B_PORT
-  IO_DIR_IN( SENSOR_B );
-  IO_PIN_HIGH( SENSOR_B ); // pullup
+    IO_DIR_IN( SENSOR_B );
+    IO_PIN_HIGH( SENSOR_B ); // pullup
 #endif
-
+    
 #ifdef SENSOR_C_PORT
-  IO_DIR_IN( SENSOR_C );
-  IO_PIN_HIGH( SENSOR_C ); // pullup
+    IO_DIR_IN( SENSOR_C );
+    IO_PIN_HIGH( SENSOR_C ); // pullup
 #endif
-
-// setup ADC, ADMUX input to ADC6, Vref = AVcc, start , 125khz clock
-// free running mode
-  adc_mux( IO_ADCMUX( SENSOR_A ) );
-  
-  ADCSRA = _BV( ADEN ) | _BV( ADSC ) | _BV( ADPS0 ) | _BV( ADPS1 ) | _BV( ADPS2 ) | _BV( ADATE );
-
-  DIDR0 =
+    
+    // setup ADC, ADMUX input to ADC6, Vref = AVcc, start , 125khz clock
+    // free running mode
+    adc_mux( IO_ADCMUX( SENSOR_A ) );
+    
+    ADCSRA = _BV( ADEN ) | _BV( ADSC ) | _BV( ADPS0 ) | _BV( ADPS1 ) | _BV( ADPS2 ) | _BV( ADATE );
+    
+    DIDR0 =
 #ifdef SENSOR_A_PORT
-  _BV( IO_DIDR0( SENSOR_A ) ) |
+    _BV( IO_DIDR0( SENSOR_A ) ) |
 #endif
 #ifdef SENSOR_B_PORT
-  _BV( IO_DIDR0( SENSOR_B ) ) |
+    _BV( IO_DIDR0( SENSOR_B ) ) |
 #endif
 #ifdef SENSOR_C_PORT
-  _BV( IO_DIDR0( SENSOR_C ) ) |
+    _BV( IO_DIDR0( SENSOR_C ) ) |
 #endif
-  0;
+    0;
 }
 
 static void IO_Init()
 {
-  IO_DIR_OUT( LED_A );
-  IO_DIR_OUT( LED_B );
-  
-  IO_DIR_OUT( RELAY_HEATER );
-  IO_DIR_OUT( RELAY_FAN );
-  IO_DIR_OUT( RELAY_AIR );
-  IO_DIR_OUT( RELAY_SPARK );
-
-#ifdef BUTTON_A_PORT  
-  IO_DIR_IN( BUTTON_A );
-  IO_PIN_HIGH( BUTTON_A ); // pullup
+    IO_DIR_OUT( LED_A );
+    IO_DIR_OUT( LED_B );
+    
+    IO_DIR_OUT( RELAY_HEATER );
+    IO_DIR_OUT( RELAY_FAN );
+    IO_DIR_OUT( RELAY_AIR );
+    IO_DIR_OUT( RELAY_SPARK );
+    
+#ifdef BUTTON_A_PORT
+    IO_DIR_IN( BUTTON_A );
+    IO_PIN_HIGH( BUTTON_A ); // pullup
 #endif
 #ifdef BUTTON_B_PORT
-  IO_DIR_IN( BUTTON_B );
-  IO_PIN_HIGH( BUTTON_B ); // pullup
+    IO_DIR_IN( BUTTON_B );
+    IO_PIN_HIGH( BUTTON_B ); // pullup
 #endif
 }
 
 int main(void)
 {
-  wdt_enable(WDTO_1S);
-  
-  USB_Init();
-  
-  IO_Init();
-  
-
-  RELAY_OFF( RELAY_HEATER );
-  RELAY_OFF( RELAY_FAN );
-  RELAY_OFF( RELAY_AIR );
-  RELAY_OFF( RELAY_SPARK );
-  
-  LED_ON( LED_A );
-  LED_ON( LED_B );
-
-  init_sensors();
-  
-  twi_init();
-  lcd_init();
-  
-  uint32_t timer = 0;
-  uint32_t burning = 0;
-  uint32_t status = 0;
-  uint8_t ignition_count = 0;
-
-  state_t state = state_init;
-
+    wdt_enable(WDTO_1S);
+    
+    USB_Init();
+    
+    IO_Init();
+    
+    
+    RELAY_OFF( RELAY_HEATER );
+    RELAY_OFF( RELAY_FAN );
+    RELAY_OFF( RELAY_AIR );
+    RELAY_OFF( RELAY_SPARK );
+    
+    LED_ON( LED_A );
+    LED_ON( LED_B );
+    
+    init_sensors();
+    
+    twi_init();
+    lcd_init();
+    
+    uint32_t timer = 0;
+    uint32_t burning = 0;
+    uint32_t status = 0;
+    uint32_t lcd_reinit = 0;
+    uint8_t ignition_count = 0;
+    
+    state_t state = state_init;
+    
 #ifdef BUTTON_A_PORT
-  int button_a = 0;
+    int button_a = 0;
 #endif
 #ifdef BUTTON_B_PORT
-  int button_b = 0;
+    int button_b = 0;
 #endif
-  
-  uint16_t sensor_a = 0;
-  uint16_t sensor_b = 0;
-  uint16_t sensor_c = 0;
-  
-  int sensor = 0;
-  float oil_temperature = 0;
-  float water_temperature = 0;
     
-  GlobalInterruptEnable();
-
-  VCP_Printf("Booting\r\n");
-
-  for(;;)
-  {
-    wdt_reset();
+    uint16_t sensor_a = 0;
+    uint16_t sensor_b = 0;
+    uint16_t sensor_c = 0;
     
-    USB_USBTask();
-    VCP_Task();
+    int sensor = 0;
+    float oil_temperature = 0;
+    float water_temperature = 0;
     
-    handle_led();
+    GlobalInterruptEnable();
     
-    switch(sensor) {
-      default:
-        sensor = 0;
-      case 0:
-        adc_mux( IO_ADCMUX( SENSOR_A ) );
-        break;
-      case 1:
-        adc_mux( IO_ADCMUX( SENSOR_B ) );
-        break;
-      case 2:
-        adc_mux( IO_ADCMUX( SENSOR_C ) );
-    }
+    VCP_Printf("Booting\r\n");
     
-    _delay_ms(TICK_MS);
-    
-    switch(sensor) {
-      case 0:
-        sensor_a = ADC;
-        break;
-      case 1:
-        sensor_b = ADC;
-        break;
-      case 2:
-        sensor_c = ADC;
-        break;
-    }
-    
-    ++sensor;
-    
-    ++status;
-    if(status > DELAY_STATUS) {
-      oil_temperature = TEMP_SENSOR(sensor_b);
-      water_temperature = TEMP_SENSOR(sensor_c);
-      
-      if(lcd_bus_error) {
-        lcd_init();
-      }
-      
-      VCP_Printf("State:%d [%s], s_A:%u, s_B:%u, s_C:%u, t_Oil:%d, t_Water:%d, Flame:%d IgnCount:%d\r\n", state, state_name[state], sensor_a, sensor_b, sensor_c, (int)oil_temperature, (int)water_temperature, (int)burning, (int)ignition_count);
-      status = 0;
-      lcd_move(0, 0);
-      lcd_printf("F:% 5u %-6s", sensor_a, state_name[state]);
-      lcd_move(0, 1);
-      lcd_printf("O:%3d" SYM_DEG "C W:%3d" SYM_DEG "C", (int)oil_temperature, (int)water_temperature);
-    }
-    
-
-#ifdef BUTTON_A_PORT    
-    if( !IO_PIN_READ( BUTTON_A ) ) {
-      if(button_a < BUTTON_DEBOUNCE) {
-        ++button_a;
-      }
-    } else {
-      button_a = 0;
-    }
-#endif
-
-#ifdef BUTTON_B_PORT    
-    if( !IO_PIN_READ( BUTTON_B ) ) {
-      if(button_b < BUTTON_DEBOUNCE) {
-        ++button_b;
-      }
-    } else {
-      button_b = 0;
-    }
-#endif    
-    
-    // flame state machine
-    switch(state)
+    for(;;)
     {
-      case state_init:
-        led_a = blink_off;
-        led_b = blink_slow;
-        state = state_wait;
-        timer = 0;
+        wdt_reset();
         
-        // turn off relays
-        RELAY_OFF( RELAY_FAN );
-        RELAY_OFF( RELAY_AIR );
-        RELAY_OFF( RELAY_SPARK );
+        USB_USBTask();
+        VCP_Task();
         
-      break;
-    
-      case state_wait:
-        if(IS_OIL_HOT() && IS_WATER_COLD()) {
-           RELAY_ON( RELAY_FAN );
-           state = state_fan;
-           timer = 0;
-           ++ignition_count;
+        handle_led();
+        
+        switch(sensor) {
+            default:
+                sensor = 0;
+            case 0:
+                adc_mux( IO_ADCMUX( SENSOR_A ) );
+                break;
+            case 1:
+                adc_mux( IO_ADCMUX( SENSOR_B ) );
+                break;
+            case 2:
+                adc_mux( IO_ADCMUX( SENSOR_C ) );
         }
-      break;
-
-      case state_fan:
-        if( ++timer  > TIMEOUT_FAN )
+        
+        _delay_ms(TICK_MS);
+        
+        switch(sensor) {
+            case 0:
+                sensor_a = ADC;
+                break;
+            case 1:
+                sensor_b = ADC;
+                break;
+            case 2:
+                sensor_c = ADC;
+                break;
+        }
+        
+        ++sensor;
+        
+        ++status;
+        if(status > DELAY_STATUS) {
+            if(lcd_reinit == 0) {
+                lcd_bus_error = 1; // force LCD bus error
+                lcd_reinit = LCD_REINIT_COUNT;
+            }
+            
+            --lcd_reinit;
+            
+            oil_temperature = TEMP_SENSOR(sensor_b);
+            water_temperature = TEMP_SENSOR(sensor_c);
+            
+            if(lcd_bus_error) {
+                lcd_init();
+            }
+            
+            VCP_Printf("State:%d [%s], s_A:%u, s_B:%u, s_C:%u, t_Oil:%d, t_Water:%d, Flame:%d IgnCount:%d\r\n", state, state_name[state], sensor_a, sensor_b, sensor_c, (int)oil_temperature, (int)water_temperature, (int)burning, (int)ignition_count);
+            status = 0;
+            lcd_move(0, 0);
+            lcd_printf("F:% 5u %-6s", sensor_a, state_name[state]);
+            lcd_move(0, 1);
+            lcd_printf("O:%3d" SYM_DEG "C W:%3d" SYM_DEG "C", (int)oil_temperature, (int)water_temperature);
+        }
+        
+        
+#ifdef BUTTON_A_PORT
+        if( !IO_PIN_READ( BUTTON_A ) ) {
+            if(button_a < BUTTON_DEBOUNCE) {
+                ++button_a;
+            }
+        } else {
+            button_a = 0;
+        }
+#endif
+        
+#ifdef BUTTON_B_PORT
+        if( !IO_PIN_READ( BUTTON_B ) ) {
+            if(button_b < BUTTON_DEBOUNCE) {
+                ++button_b;
+            }
+        } else {
+            button_b = 0;
+        }
+#endif
+        
+        // flame state machine
+        switch(state)
         {
-          RELAY_ON( RELAY_AIR );
-          state = state_air;
-          timer = 0;
+            case state_init:
+                led_a = blink_off;
+                led_b = blink_slow;
+                state = state_wait;
+                timer = 0;
+                
+                // turn off relays
+                RELAY_OFF( RELAY_FAN );
+                RELAY_OFF( RELAY_AIR );
+                RELAY_OFF( RELAY_SPARK );
+                
+                break;
+                
+            case state_wait: // This state is waiting for water to cool down and button_b (external thermostat?) to be pressed.
+                if(IS_WATER_COLD() && IS_PRESSED(button_b)) {
+                    state = state_preheat;
+                }
+                break;
+                
+            case state_preheat: // waiting for oil to be *not* cold (hot oil trigger is for oil heating state machine)
+                if(!IS_OIL_COLD()) {
+                    RELAY_ON( RELAY_FAN );
+                    state = state_fan;
+                    timer = 0;
+                    ++ignition_count;
+                }
+                break;
+                
+            case state_fan:
+                if( ++timer  > TIMEOUT_FAN )
+                {
+                    RELAY_ON( RELAY_AIR );
+                    state = state_air;
+                    timer = 0;
+                }
+                break;
+                
+            case state_air:
+                if( ++timer > TIMEOUT_AIR ) {
+                    state = state_spark;
+                    RELAY_ON( RELAY_SPARK );
+                    led_a = blink_fast;
+                    timer = 0;
+                }
+                break;
+                
+            case state_spark:
+                if( ++timer > TIMEOUT_SPARK )
+                {
+                    RELAY_OFF( RELAY_SPARK );
+                    state = state_detect_flame;
+                    led_a = blink_off;
+                    led_b = blink_fast;
+                    timer = 0;
+                }
+                break;
+            case state_detect_flame:
+                if( ++timer > TIMEOUT_FLAME )
+                {
+                    if(ignition_count > IGNITION_RETRY) {
+                        state = state_fault;
+                    } else {
+                        state = state_init;
+                    }
+                    timer = 0;
+                }
+                else if( IS_BURNING() )
+                {
+                    led_a = blink_off;
+                    led_b = blink_slow;
+                    
+                    state = state_burn;
+                    timer = 0;
+                    ignition_count = 0;
+                }
+                break;
+            case state_burn:
+                if( IS_BURNING() ) {
+                    burning = FLAME_SENSOR_DELAY;
+                } else if(burning) {
+                    --burning;
+                }
+                
+                if(burning == 0) {
+                    state = state_init;
+                }
+                break;
+            case state_fault:
+                RELAY_OFF( RELAY_FAN );
+                RELAY_OFF( RELAY_AIR );
+                RELAY_OFF( RELAY_SPARK );
+                
+                led_a = blink_slow;
+                led_b = blink_slow;
+                
+                break;
+                
         }
-        break;
-
-      case state_air:
-        if( ++timer > TIMEOUT_AIR ) {
-            state = state_spark;
-            RELAY_ON( RELAY_SPARK );
-            led_a = blink_fast;
-            timer = 0;
-        }
-        break;
-
-      case state_spark:
-        if( ++timer > TIMEOUT_SPARK )
+        
+        // heater state machine
+        switch(state)
         {
-          RELAY_OFF( RELAY_SPARK );
-          state = state_detect_flame;
-          led_a = blink_off;
-          led_b = blink_fast;
-          timer = 0;
+            case state_init:
+            case state_wait:
+            case state_fault:
+                RELAY_OFF( RELAY_HEATER );
+                break;
+            default:
+                if(IS_OIL_HOT()) {
+                    RELAY_OFF( RELAY_HEATER );
+                }
+                if(IS_OIL_COLD()) {
+                    RELAY_ON( RELAY_HEATER );
+                }
+                break;
         }
-        break;
-      case state_detect_flame:
-        if( ++timer > TIMEOUT_FLAME )
-        {
-          if(ignition_count > IGNITION_RETRY) {
-            state = state_fault;
-          } else {
+        
+        // safety:
+        
+#ifdef BUTTON_A_PORT
+        // soft reset
+        if(IS_PRESSED(button_a)) {
             state = state_init;
-          }
-          timer = 0;
         }
-        else if( IS_BURNING() )
-        {
-          led_a = blink_off;
-          led_b = blink_slow;
-          
-          state = state_burn;
-          timer = 0;
-          ignition_count = 0;
-        }
-        break;
-      case state_burn:
-        if( IS_BURNING() ) {
-          burning = FLAME_SENSOR_DELAY;
-        } else if(burning) {
-          --burning;
-        }
-        
-        if(burning == 0) {
-          state = state_init;
-        }
-
-//        if( IS_OIL_COLD() ) {
-//          state = state_init;
-//        }
-
-        if( IS_WATER_HOT() ) {
-          state = state_init;
-        }
-
-        break;
-      case state_fault:
-        RELAY_OFF( RELAY_FAN );
-        RELAY_OFF( RELAY_AIR );
-        RELAY_OFF( RELAY_SPARK );
-
-        led_a = blink_slow;
-        led_b = blink_slow;
-
-        break;
-
-    }
-    
-    // heater state machine
-    switch(state)
-    {
-      case state_init:
-      case state_fault:
-        RELAY_OFF( RELAY_HEATER );
-        break;
-      default:
-        if(IS_OIL_HOT()) {
-          RELAY_OFF( RELAY_HEATER );
-        }
-        if(IS_OIL_COLD()) {
-          RELAY_ON( RELAY_HEATER );
-        }
-        break;
-    }
-    
-#ifdef BUTTON_A_PORT
-    if(IS_PRESSED(button_a)) {
-      state = state_init;
-    }
 #endif
 #ifdef BUTTON_B_PORT
-    if(IS_PRESSED(button_b)) {
-      state = state_fault;
-    }
+        // external thermostat
+        if(!IS_PRESSED(button_b)) {
+            state = state_init;
+        }
 #endif
+        // water hot
+        if(IS_WATER_HOT()) {
+            state = state_init;
+        }
+        
+    }
     
-  }
-
-
+    
 }
